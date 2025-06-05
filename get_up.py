f73f701a0f9ac471c4903b3dda56992eb12d7671
nativeas <saicn@qq.com>
2025-06-05 18:31:29 +0800

    venv

--- get_up.py
+++ get_up.py
@@ -1,29 +1,38 @@
 import argparse
-import requests
+import os
+import random
+
 import pendulum
-
-
+import requests
+import telebot
+from BingImageCreator import ImageGen
 from github import Github
+from openai import OpenAI
+from telebot.types import InputMediaPhoto
 
-# 14 for test 12 real get up
-GET_UP_ISSUE_NUMBER = 1   
+# 20 for test 12 real get up
+GET_UP_ISSUE_NUMBER = 12
 GET_UP_MESSAGE_TEMPLATE = (
-    "今天的起床时间是--{get_up_time}.\r\n\r\n 懒猪🐷起床啦，赶紧去跑步，上班不迟到。\r\n\r\n 今天的一句诗:\r\n {sentence}"
+    "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n"
 )
 SENTENCE_API = "https://v1.jinrishici.com/all"
 DEFAULT_SENTENCE = "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
 TIMEZONE = "Asia/Shanghai"
+client = OpenAI()
 
 
 def login(token):
     return Github(token)
 
 
-def get_one_sentence():
+def get_one_sentence(up_list):
     try:
         r = requests.get(SENTENCE_API)
         if r.ok:
-            return r.json().get("content", DEFAULT_SENTENCE)
+            concent = r.json().get("content")
+            if concent in up_list:
+                return get_one_sentence(up_list)
+            return concent
         return DEFAULT_SENTENCE
     except:
         print("get SENTENCE_API wrong")
@@ -34,51 +43,99 @@ def get_today_get_up_status(issue):
     comments = list(issue.get_comments())
     if not comments:
         return False
+    up_list = []
+    for comment in comments:
+        try:
+            s = comment.body.splitlines()[6]
+            up_list.append(s)
+        except Exception as e:
+            print(str(e), "!!")
+            continue
     latest_comment = comments[-1]
     now = pendulum.now(TIMEZONE)
     latest_day = pendulum.instance(latest_comment.created_at).in_timezone(
         "Asia/Shanghai"
     )
     is_today = (latest_day.day == now.day) and (latest_day.month == now.month)
-    return is_today
+    return is_today, up_list
 
 
-def make_get_up_message():
-    sentence = get_one_sentence()
+def make_pic_and_save(sentence, bing_cookie):
+    # for bing image when dall-e3 open drop this function
+    i = ImageGen(bing_cookie)
+    prompt = f"revise `{sentence}` to a DALL-E prompt"
+    completion = client.chat.completions.create(
+        messages=[{"role": "user", "content": prompt}],
+        model="gpt-4-1106-preview",
+    )
+    sentence = completion.choices[0].message.content.encode("utf8").decode()
+    print(f"revies: {sentence}")
+
+    images = i.get_images(sentence)
+    date_str = pendulum.now().to_date_string()
+    new_path = os.path.join("OUT_DIR", date_str)
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+    i.save_images(images, new_path)
+    index = random.randint(0, len(images) - 1)
+    image_url_for_issue = f"https://github.com/yihong0618/2023/blob/main/OUT_DIR/{date_str}/{index}.jpeg?raw=true"
+    return images, image_url_for_issue
+
+
+def make_get_up_message(bing_cookie, up_list):
+    sentence = get_one_sentence(up_list)
     now = pendulum.now(TIMEZONE)
-    # 3 - 7 means early for me
-    is_get_up_early = 6 <= now.hour <= 18    
+    # 3 - 8 means early for me
+    is_get_up_early = 3 <= now.hour <= 8
     get_up_time = now.to_datetime_string()
+    link_list = []
+    try:
+        link_list, link_for_issue = make_pic_and_save(sentence, bing_cookie)
+    except Exception as e:
+        print(str(e))
+        # give it a second chance
+        try:
+            sentence = get_one_sentence(up_list)
+            print(f"Second: {sentence}")
+            link_list, link_for_issue = make_pic_and_save(sentence, bing_cookie)
+        except Exception as e:
+            print(str(e))
     body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
-    return body, is_get_up_early
+    print(body, link_list, link_for_issue)
+    return body, is_get_up_early, link_list, link_for_issue
 
 
-def main(github_token, repo_name, weather_message, tele_token, tele_chat_id):
+def main(
+    github_token,
+    repo_name,
+    bing_cookie,
+    weather_message,
+    tele_token,
+    tele_chat_id,
+):
     u = login(github_token)
     repo = u.get_repo(repo_name)
     issue = repo.get_issue(GET_UP_ISSUE_NUMBER)
-    is_toady = get_today_get_up_status(issue)
-    if is_toady:
+    is_today, up_list = get_today_get_up_status(issue)
+    if is_today:
         print("Today I have recorded the wake up time")
         return
-    early_message, is_get_up_early = make_get_up_message()
+    early_message, is_get_up_early, link_list, link_for_issue = make_get_up_message(
+        bing_cookie, up_list
+    )
     body = early_message
     if weather_message:
         weather_message = f"现在的天气是{weather_message}\n"
         body = weather_message + early_message
     if is_get_up_early:
-        issue.create_comment(body)
+        comment = body + f"![image]({link_for_issue})"
+        issue.create_comment(comment)
         # send to telegram
         if tele_token and tele_chat_id:
-            requests.post(
-                url="https://api.telegram.org/bot{0}/{1}".format(
-                    tele_token, "sendMessage"
-                ),
-                data={
-                    "chat_id": tele_chat_id,
-                    "text": body,
-                },
-            )
+            bot = telebot.TeleBot(tele_token)
+            photos_list = [InputMediaPhoto(i) for i in link_list]
+            photos_list[0].caption = body
+            bot.send_media_group(tele_chat_id, photos_list, disable_notification=True)
     else:
         print("You wake up late")
 
@@ -90,12 +147,18 @@ if __name__ == "__main__":
     parser.add_argument(
         "--weather_message", help="weather_message", nargs="?", default="", const=""
     )
-    parser.add_argument("--tele_token", help="tele_token", nargs="?", default="", const="")
-    parser.add_argument("--tele_chat_id", help="tele_chat_id", nargs="?", default="", const="")
+    parser.add_argument("bing_cookie", help="bing cookie")
+    parser.add_argument(
+        "--tele_token", help="tele_token", nargs="?", default="", const=""
+    )
+    parser.add_argument(
+        "--tele_chat_id", help="tele_chat_id", nargs="?", default="", const=""
+    )
     options = parser.parse_args()
     main(
         options.github_token,
         options.repo_name,
+        options.bing_cookie,
         options.weather_message,
         options.tele_token,
         options.tele_chat_id,
